192ea7a3ce778acc2762ebc21085aaab8c6ab9d6
mxz-dev <zabetzadehmohmmad@gmail.com>
2025-01-04 16:15:33 +0200

    stage for profile list ...

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+
+app_name = 'twitter'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profiles/', views.profiles , name='profiles'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from twitter.models import Profile
 def index(request):
     pass
-    # return render(request, '')
+    return render(request, 'home.html')
+
+def profiles(request):
+    profiles = Profile.objects.exclude(user=request.user.id)
+    return render(request, 'twitter/profiles.html', {'profiles': profiles})
