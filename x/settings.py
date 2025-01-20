cec5503f3afffd67896f24e1651d714ce0c9e677
mxz-dev <zabetzadehmohmmad@gmail.com>
2025-01-20 16:33:47 +0200

    added volume

--- x/settings.py
+++ x/settings.py
@@ -69,7 +69,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'db/db.sqlite3',
         }
     }
 else:
